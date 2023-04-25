245f285faa8db90ebb1a8eb889b4d78f55591a50
MergimVeselaj <mergim.veselaj@student.odisee.be>
2023-04-25 12:16:47 +0200

    Les1

---
+++ schooladministatie/School.java
@@ -0,0 +1,3 @@
+public class School {
+    public static ArrayList<Student> studenten = new ArrayList<>();
+}
---
+++ schooladministatie/Student.java
@@ -0,0 +1,10 @@
+public class Student {
+
+    private String voornaam;
+    private String achternaam;
+
+    public Student(String voornaam, String achternaam){
+        this.voornaam = voornaam;
+        this.achternaam = achternaam;
+    }
+}
